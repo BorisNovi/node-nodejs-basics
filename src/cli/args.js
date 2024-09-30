b9f2389965530046a9e4990608cb53cffbc33d14
Boris Novikov <borisnovikov4815@gmail.com>
2024-10-01 00:20:52 +0400

    feat/refactor: read args / refactor esm.mjs as es module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,6 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
-    console.log('args');
+  const args = process.argv;
+  const pathlessArgs = args.slice(2);
+
+  for (let i = 0; i <= pathlessArgs.length; i += 2) {
+      const key = args[i].replace('--', '');
+      const value = args[i + 1];
+      console.log(`${key} is ${value}`);
+    }
 };
 
 parseArgs();
