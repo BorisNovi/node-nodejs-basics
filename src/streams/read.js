515142c9f2171cc24ec2d70c392e226521ac6cfd
Boris Novikov <borisnovikov4815@gmail.com>
2024-10-02 00:57:47 +0400

    feat: change esm and add solution for streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = resolve(__filename, '../files/fileToRead.txt'); 
+
+  console.log(__dirname);  
+  const readStream = createReadStream(__dirname, { encoding: 'utf8' });  
+  const hexReadStream = createReadStream(__dirname, { encoding: 'ascii' });
+
+  readStream.pipe(process.stdout);  
+  hexReadStream.pipe(process.stdout);
 };
 
 await read();
